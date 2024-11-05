e0e339d468e46c0d509db9d2ede8af4f1308b834
Pedro <plyra1812@gmail.com>
2024-11-05 18:31:20 -0300

    soma finalizado

---
+++ main.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+  function soma() {
+    count++;
+    document.getElementById("counter").textContent = count;
+  }
+
+  function subt() {
+    count--;
+    document.getElementById("counter").textContent = count;
+  }
